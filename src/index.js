40ffb74bc7f53c25f02da62185cdaf3cd3ca0577
sagarbhade <bpsaagar@gmail.com>
2023-01-09 00:54:14 +0530

    first react proj

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+const Person = ({ img, name, job, gender, children }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/${gender}/${img}.jpg`
+  return (
+    <article className="person">
+      <img src={url} alt="person" />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </article>
+  )
+}
+
+const PersonList = () => {
+  return (
+    <section className="person-list">
+      <Person img="34" name="john" job="developer" gender="men" />
+      <Person img="65" name="maria" job="designer" gender="women">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores,
+          tempora!
+        </p>
+      </Person>
+      <Person img="61" name="david" job="the boss" gender="men" />
+    </section>
+  )
+}
+
+ReactDOM.render(<PersonList />, document.getElementById('root'))
